52fe800f9005a164ac2a0d0c122844d656d4ed1e
RaresDrg <dragan_rares13@yahoo.com>
2024-07-16 15:01:04 +0000

    Deploying to gh-pages from @ RaresDrg/Health-React-Node.js-project@57c15d65448bc265b7442a0c64b66ebcc1168c04 🚀

---
+++ static/js/187.66019642.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkHealth_React_Node_js_project=self.webpackChunkHealth_React_Node_js_project||[]).push([[187],{2187:function(t,e,r){r.d(e,{Csy:function(){return a}});var i=r(2119);function a(t){return(0,i.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{fill:"none",strokeLinecap:"square",strokeMiterlimit:"10",strokeWidth:"32",d:"m112 352-64-64 64-64"},child:[]},{tag:"path",attr:{fill:"none",strokeLinecap:"square",strokeMiterlimit:"10",strokeWidth:"32",d:"M64 288h400V160"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=187.66019642.chunk.js.map
