5ef0b52acc93d2fc7bd58ea0681359000f363209
RaresDrg <dragan_rares13@yahoo.com>
2024-07-16 18:10:14 +0300

    Done
    Done

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,8 +1,11 @@
-import logo from "../../assets/logo.svg";
+import logo from '../../assets/logo.svg';
 
 const Logo = ({ className: styles }) => {
   return (
-    <a className={styles} href="/">
+    <a
+      className={styles}
+      href="https://raresdrg.github.io/Health-React-Node.js-project/"
+    >
       <img src={logo} alt="app-logo" />
       <p>
         <span>Health</span>
